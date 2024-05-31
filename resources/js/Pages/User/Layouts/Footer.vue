1b15bf1f3b1e6557e4bc1687a62f6ddc8a5b2104
unknown <me@here.com>
2024-05-31 14:11:32 +0700

    first commit

---
+++ resources/js/Pages/User/Layouts/Footer.vue
@@ -0,0 +1,100 @@
+<template>
+    <footer class="mt-12 bg-gray-50">
+        <div
+            class="grid max-w-screen-xl px-4 py-10 mx-auto gap-y-8 gap-x-12 md:grid-cols-2 xl:grid-cols-4 xl:px-10"
+        >
+            <div class="max-w-sm">
+                <div class="flex items-center h-12 mb-6 space-x-2">
+                    <span class="text-2xl font-bold"
+                        >Etra<span class="text-blue-600">de</span>.</span
+                    >
+                </div>
+                <div class="text-gray-500">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Nobis ad a officia ea expedita!
+                </div>
+            </div>
+            <div class="">
+                <div class="mt-4 mb-2 font-medium xl:mb-4">Address</div>
+                <div class="text-gray-500">
+                    35 Remida Heights, <br />
+                    45 Street, <br />
+                    South Caroline, US
+                </div>
+            </div>
+            <div class="">
+                <div class="mt-4 mb-2 font-medium xl:mb-4">Links</div>
+                <nav aria-label="Footer Navigation" class="text-gray-500">
+                    <ul class="space-y-3">
+                        <li>
+                            <a
+                                class="hover:text-blue-600 hover:underline"
+                                href="#"
+                                >Pricing</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                class="hover:text-blue-600 hover:underline"
+                                href="#"
+                                >Demo</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                class="hover:text-blue-600 hover:underline"
+                                href="#"
+                                >Press</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                class="hover:text-blue-600 hover:underline"
+                                href="#"
+                                >Support Hub</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                class="hover:text-blue-600 hover:underline"
+                                href="#"
+                                >Contact</a
+                            >
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="">
+                <div class="mt-4 mb-2 font-medium xl:mb-4">
+                    Subscribe to our Newsletter
+                </div>
+                <div class="flex flex-col">
+                    <div class="mb-4">
+                        <input
+                            type="email"
+                            class="block w-full px-4 mb-2 bg-gray-200 focus:outline h-14 rounded-xl sm:w-80 focus:outline-none focus:ring-1 focus:ring-blue-600"
+                            placeholder="Enter your email"
+                        />
+                        <button
+                            class="block px-6 py-3 font-medium text-white bg-blue-600 rounded-xl"
+                        >
+                            Subscribe
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="bg-gray-100">
+            <div
+                class="flex flex-col max-w-screen-xl px-4 py-3 mx-auto text-center text-gray-500 gap-y-4 sm:flex-row sm:justify-between sm:text-left"
+            >
+                <div class="">© 2022 Etrade | All Rights Reserved</div>
+                <div class="">
+                    <a class="" href="#">Privacy Policy</a>
+                    <span>|</span>
+                    <a class="" href="#">Terms of Service</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
---
+++ resources/js/Pages/User/Layouts/Hero.vue
@@ -0,0 +1,39 @@
+<template>
+    <div
+        class="relative h-screen overflow-hidden text-white bg-gradient-to-r from-purple-600 to-blue-600"
+    >
+        <div class="absolute inset-0">
+            <img
+                src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxjb2RlfGVufDB8MHx8fDE2OTQwOTg0MTZ8MA&ixlib=rb-4.0.3&q=80&w=1080"
+                alt="Background Image"
+                class="object-cover object-center w-full h-full"
+            />
+            <div class="absolute inset-0 bg-black opacity-50"></div>
+        </div>
+
+        <div
+            class="relative z-10 flex flex-col items-center justify-center h-full text-center"
+        >
+            <h1 class="mb-4 text-5xl font-bold leading-tight">
+                Welcome to Our Awesome Website
+            </h1>
+            <p class="mb-8 text-lg text-gray-300">
+                Discover amazing features and services that await you.
+            </p>
+            <a
+                href="#"
+                class="px-6 py-2 text-lg font-semibold text-white transition duration-300 ease-in-out transform bg-blue-800 rounded-full hover:bg-blue-700 hover:scale-105 hover:shadow-lg"
+                >Get Started</a
+            >
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import { initCarousels } from "flowbite";
+
+onMounted(() => {
+    initCarousels();
+});
+</script>
